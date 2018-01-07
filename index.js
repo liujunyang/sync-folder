fe60b8da5300d58064fd6cb2cdb1299e4c5e2ae6
liujunyang <liujunyangscript@gmail.com>
2018-01-07 16:28:45 +0800

    layout

---
+++ index.js
@@ -0,0 +1,13 @@
+const syncLocalFiles = require('./lib/local-syncfiles')
+const watchLocalFiles = require('./lib/local-watch')
+
+
+module.exports = (srcDir, targetDir, {type = 'hardlink', exclude = null, watch = false} = {}) => {
+	syncLocalFiles(srcDir, targetDir, {type, exclude})
+
+	if (watch) {
+		const watcher = watchLocalFiles(srcDir, targetDir, {type, exclude})
+
+		return watcher
+	}
+}
